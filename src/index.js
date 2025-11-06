cf4d8e93fc9ca8518688e29e48ccd575850be1b8
ShanmukhaGaneshRepaka <shanmukhafganeshrepaka@gmail.com>
2025-11-06 18:44:38 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,38 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-const backgroundImage = '/images/backgrounds/New-bg.jpg';
 
-
-// // Function to load the Tidio script
-// function loadTidioScript() {
-//   const script = document.createElement('script');
-//   script.src = "//code.tidio.co/wpu5hre78erukppaxms2pcge39ckekpy.js";
-//   script.async = true;
-//   document.body.appendChild(script);
-// }
-
-// Load the Tidio script when the DOM is fully loaded
-// document.addEventListener('DOMContentLoaded', loadTidioScript);
- 
-// Apply background to the root element
-document.body.style.backgroundImage = `url(${backgroundImage})`;
-document.body.style.backgroundSize = 'cover';
-document.body.style.backgroundPosition = 'center';
-document.body.style.backgroundAttachment = 'fixed';
-document.body.style.backgroundRepeat = 'no-repeat';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="33884279909-pupqt6eev817ebnudqfgar1ei8bqtbck.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>
 );
- 
-reportWebVitals();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
